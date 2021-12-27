d99807e779d3b72b5b10b39c741209b74b1d50c1
Matheus-Gondra <matheusgondra951@gmail.com>
2021-12-27 20:41:19 -0300

    Criando a lógica da função handleRemoveTask

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,26 +16,52 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
-    let newTask = [...tasks,{id: 0, title:"", isComplete: false}];
+    let newTask = [...tasks, { id: 0, title: "", isComplete: false }];
 
     newTask.map((task, index) => {
       newTask = [...tasks, { id: index, title: newTaskTitle, isComplete: false }];
       setTasks(newTask);
+      setNewTaskTitle("");
       return;
     });
 
-    
-   
+
+    /* Solução do professor
+      if(!newTaskTitle) return;
+
+      const newTask = {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false
+      };
+
+      setTasks(oldState => [...oldState, newTask]);
+      setNewTaskTitle("";)
+    */
+
+
+
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    const newTasks = tasks.map(task => task.id === id ? {...task, isComplete: !task.isComplete} : task);
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    // Remova uma task da listagem pelo ID=
+    setTasks(tasks.filter(task => task.id !== id));
+
+    /* Solução do professor
     
-    setTasks(tasks.filter(task => task.id !== id ));
+    const filteredTasks = task.filter(task => task.id !== id);
+
+    setTasks(filteredTasks);
+    
+    */
   }
 
   return (
